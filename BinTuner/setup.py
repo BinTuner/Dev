99550fca79e099b965dfe3bddc1319ac9bdb45f9
BinTuner <63747499+BinTuner@users.noreply.github.com>
2021-12-26 23:33:50 +0800

    Add files via upload

---
+++ BinTuner/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+try:
+    from setuptools import setup
+except ImportError:
+    try:
+        from setuptools.core import setup
+    except ImportError:
+        from distutils.core import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rest')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+required = open('requirements.txt').read().splitlines()
+required = [l.strip() for l in required
+            if l.strip() and not l.strip().startswith('#')]
+
+setup(
+    name='opentuner',
+    version='0.8.3',
+    url='http://opentuner.org/',
+    license='MIT',
+    author='Jason Ansel',
+    author_email='jansel@jansel.net',
+    description='An extensible framework for program autotuning',
+    long_description=read_md('README.md'),
+    packages=['opentuner', 'opentuner.resultsdb', 'opentuner.utils',
+              'opentuner.measurement', 'opentuner.search'],
+    install_requires=required,
+)
